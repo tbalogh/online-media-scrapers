15cf10d0c6925bb79dfca4e63b90c5cdf00bda49
Tamas Balogh <Tamas.Balogh@logmein.com>
2018-03-16 15:51:14 +0100

    Remove unused code

--- online_media_scrapers/spiders/index/test_index_daily_archive_url_provider.py
+++ online_media_scrapers/spiders/index/test_index_daily_archive_url_provider.py
@@ -11,22 +11,22 @@ class TestIndexDailyArchiveUrlProvider(unittest.TestCase):
 
     def test_provide_index_urls_between_dates(self):
         test_map = {
-            ("2017/02/28", "2017/03/01", 2): [
+            ("2017/02/28", "2017/03/01"): [
                 'http://index.hu/belfold/2017/02/28', 'http://index.hu/kulfold/2017/02/28',
                  'http://index.hu/gazdasag/2017/02/28',
                 'http://index.hu/belfold/2017/03/01', 'http://index.hu/kulfold/2017/03/01',
                  'http://index.hu/gazdasag/2017/03/01'
             ],
-            ("2016/12/31", "2017/01/01", 2): [
+            ("2016/12/31", "2017/01/01"): [
                 'http://index.hu/belfold/2016/12/31', 'http://index.hu/kulfold/2016/12/31',
                  'http://index.hu/gazdasag/2016/12/31',
                 'http://index.hu/belfold/2017/01/01', 'http://index.hu/kulfold/2017/01/01',
                  'http://index.hu/gazdasag/2017/01/01',
             ],
-            ("2016/12/01", "2016/12/01", 1): ['http://index.hu/belfold/2016/12/01',
+            ("2016/12/01", "2016/12/01"): ['http://index.hu/belfold/2016/12/01',
                                               'http://index.hu/kulfold/2016/12/01',
                                               'http://index.hu/gazdasag/2016/12/01']
         }
-        for (start_date, end_date, days_between), expected_urls in test_map.items():
+        for (start_date, end_date), expected_urls in test_map.items():
             actual_urls = self.dailyArchiveUrlProvider.urls_between(start_date, end_date)
         self.assertEqual(sorted(actual_urls), sorted(expected_urls))
